dffa801af3d218d480711d13a8a106b301369675
Vinicius Miazaki <viniciusmiazaki@Vinicius-MacBook-Pro.local>
2021-06-10 12:15:41 -0700

    first commit

---
+++ cinza-grid.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:       Cinza Grid
+ * Plugin URI:        https://cinza.io/plugin/grid/
+ * Description:       A minimal grid plugin.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Cinza Web Design
+ * Author URI:        https://cinza.io/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+defined( 'ABSPATH' ) || exit;
